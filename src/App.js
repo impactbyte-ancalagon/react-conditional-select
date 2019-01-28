627262c8d218e47b7b54e52828a5b1b8246f301d
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-28 18:03:07 +0700

    Finish

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import provinces from './data/provinces.js'
 
 class App extends Component {
+  state = {
+    data: null,
+    province: '',
+    city: '',
+    state: ''
+  }
+
+  componentDidMount() {
+    this.setState({ data: provinces })
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const { data, ...state } = this.state
+
+    console.log(state)
+  }
+
   render() {
+    const { data, province, city, state } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <form onSubmit={this.handleSubmit}>
+        <select name="province" onChange={this.handleChange} value={province}>
+          <option disabled value="">
+            Province
+          </option>
+          {data &&
+            Object.keys(data).map((province, i) => (
+              <option value={province} key={i}>
+                {province}
+              </option>
+            ))}
+        </select>
+
+        <select name="city" onChange={this.handleChange} value={city}>
+          <option disabled value="">
+            City
+          </option>
+          {province !== '' &&
+            Object.keys(data[province]).map((city, i) => (
+              <option value={city} key={i}>
+                {city}
+              </option>
+            ))}
+        </select>
+
+        <select name="state" onChange={this.handleChange} value={state}>
+          <option disabled value="">
+            State
+          </option>
+          {city !== '' &&
+            data[province][city].map((state, i) => (
+              <option value={state} key={i}>
+                {state}
+              </option>
+            ))}
+        </select>
+
+        <button type="submit">Submit</button>
+      </form>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/data/provinces.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Aceh: {
+    'Kab A': ['Kec AA', 'Kec AB'],
+    'Kab B': ['Kec BA', 'Kec BB'],
+    'Kab C': ['Kec CA', 'Kec CB']
+  },
+  Sumut: {
+    'Kab A': ['Kec AA', 'Kec AB'],
+    'Kab B': ['Kec BA', 'Kec BB'],
+    'Kab C': ['Kec CA', 'Kec CB']
+  }
+}
